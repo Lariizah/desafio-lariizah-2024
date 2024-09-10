eae6e1e2a6b75f0a465c9ddb30e318fe79147e46
Lariizah <larissabandeira507@gmail.com>
2024-09-10 12:14:24 -0300

    Update recintos-zoo.js

--- recintos-zoo.js
+++ recintos-zoo.js
@@ -1,5 +1,13 @@
 class RecintosZoo {
-
+    construtor() {
+        //Definindo os recintos e seus atributos 
+        this.recintos = [
+            {numero; 1, bioma: 'savana', tamanho: 10, oupacao: 3, animais ['macaco'] },
+            {numero; 2, bioma: 'floresta', tamanho: 5, ocupacao: 0, animais: [] },
+            {numero; 3, bioma: 'savana e rio', tamanho: 7, ocupacao: 1, aniamais: [] },
+            {numero; 4, bioma: 'rio' tamanho: 8, ocupacao: 0, animais: [] },
+            {numero; 5, bioma: 'savana',
+    
     analisaRecintos(animal, quantidade) {
     }
 
